6f5a02a6104d45fa92f649696e6343f25a13f447
jler <472102489@qq.com>
2018-02-02 17:00:06 +0800

    视频墙组件设计：    1.树结构设计

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false
+
+Vue.use(ElementUI, VueAxios, axios)
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  components: { App },
+  template: '<App/>'
+})
